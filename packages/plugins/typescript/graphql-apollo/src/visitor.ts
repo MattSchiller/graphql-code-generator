85ffc6744716c0a8f92aa92b8c2fcc96c0ae9f21
Matt Schiller <mschiller@codecademy.com>
2021-07-21 23:13:43 -0400

    I think that's what we're looking for in the sig

--- packages/plugins/typescript/graphql-apollo/src/visitor.ts
+++ packages/plugins/typescript/graphql-apollo/src/visitor.ts
@@ -32,6 +32,7 @@ export class GraphQLApolloVisitor extends ClientSideBaseVisitor<
     const typeImport = this.config.useTypeImports ? 'import type' : 'import';
 
     this._additionalImports.push(`${typeImport} { ApolloClient } from '@apollo/client';`);
+    this._additionalImports.push(`${typeImport} * as Apollo from '@apollo/client';`);
   }
 
   public OperationDefinition(node: OperationDefinitionNode) {
@@ -69,23 +70,26 @@ export class GraphQLApolloVisitor extends ClientSideBaseVisitor<
   }
 
   public get sdkContent(): string {
-    const sdkOperations = this._operationsToInclude.map(x => {
-      const optionalVariables =
-        !x.node.variableDefinitions ||
-        x.node.variableDefinitions.length === 0 ||
-        x.node.variableDefinitions.every(v => v.type.kind !== Kind.NON_NULL_TYPE || v.defaultValue);
+    const sdkOperations = this._operationsToInclude.map(
+      ({ node, documentVariableName, operationType, operationResultType, operationVariablesTypes }) => {
+        const optionalVariables =
+          !node.variableDefinitions ||
+          node.variableDefinitions.length === 0 ||
+          node.variableDefinitions.every(v => v.type.kind !== Kind.NON_NULL_TYPE || v.defaultValue);
 
-      const documentVariableName = x.documentVariableName;
+        const operationName = node.name.value;
+        const operationGenerics = `${operationResultType}, ${operationVariablesTypes}`;
+        const optionQueryKey = operationType === 'Mutation' ? 'mutation' : 'query';
 
-      const operationName = x.node.name.value;
-      return `${camelCase(operationName)}${x.operationType}(variables${optionalVariables ? '?' : ''}: ${
-        x.operationVariablesTypes
-      }) {
-          return client.${GraphQLApolloVisitor.getApolloOperation(x.operationType)}<${
-        x.operationResultType
-      }>({variables, query: ${documentVariableName}})
+        return `${camelCase(operationName)}${operationType}(options${
+          optionalVariables ? '?' : ''
+        }: ${GraphQLApolloVisitor.getApolloOperationOptionType(operationType)}<${operationGenerics}>) {
+          return client.${GraphQLApolloVisitor.getApolloOperation(
+            operationType
+          )}<${operationGenerics}>({...options, ${optionQueryKey}: ${documentVariableName}})
       }`;
-    });
+      }
+    );
     return `export const getSdk = (client: ApolloClient<any>) => ({
       ${sdkOperations.join(',\n')}
     });
@@ -100,6 +104,19 @@ export class GraphQLApolloVisitor extends ClientSideBaseVisitor<
         return 'mutate';
       case 'Query':
         return 'query';
+      default:
+        throw new Error('unknown operation type: ' + operationType);
+    }
+  }
+
+  private static getApolloOperationOptionType(operationType: string): string {
+    switch (operationType) {
+      case 'Subscription':
+        return 'Apollo.SubscriptionOptions';
+      case 'Mutation':
+        return 'Apollo.MutationOptions';
+      case 'Query':
+        return 'Apollo.QueryOptions';
       default:
         throw new Error('unknown operation type: ' + operationType);
     }
